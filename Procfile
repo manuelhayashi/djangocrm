0ff31402e5a369b96e3b88b04c2e8b8bf436afcf
manuel hayashi <manuelhayashi@icloud.com>
2020-09-18 16:33:31 -0500

    heroku tweaks

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn mywebsite_Project2.wsgi
--- mywebsite_project2/mywebsite_project2/settings.py
+++ mywebsite_project2/mywebsite_project2/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django-heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -50,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mywebsite_project2.urls'
@@ -126,3 +131,7 @@ MEDIA_URL = ' /images/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
