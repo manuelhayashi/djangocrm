ac01ce30049d9a7bab302cf2810858c95af43289
Manuel <manuelhayashi@icloud.com>
2020-09-19 23:55:42 -0500

    'debugging'

--- mywebsite_project2/mywebsite_project2/wsgi.py
+++ mywebsite_project2/mywebsite_project2/wsgi.py
@@ -9,6 +9,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mywebsite_project2.settings')
+os.environ.setdefault['DJANGO_SETTINGS_MODULE'] = 'mywebsite_project2.settings'
 
 application = get_wsgi_application()
